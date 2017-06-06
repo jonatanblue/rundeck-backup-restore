fb9c6f8dc7fd6d97b5e942bacad482fff18484ed
Jonatan Bjork <jonatan@jonatanblue.se>
2017-06-06 16:49:01 +0100

    added restore stub

--- backup.py
+++ backup.py
@@ -6,18 +6,16 @@ from progress.bar import Bar
 
 class Keeper:
 
-    def __init__(self, destination_path, filename, show_progress):
-        self.destination_path = destination_path
-        self.filename = filename
+    def __init__(self, show_progress):
         self.count = 0
         self.bar = None
         self.show_progress = show_progress
         # Directories to include in backup and restore
         self.source_directories = [
-            "/var/lib/rundeck/data",            # database
-            "/var/lib/rundeck/logs",            # execution logs (by far biggest)
-            "/var/lib/rundeck/.ssh",            # ssh keys
-            "/var/lib/rundeck/var/storage",     # key storage files and metadata
+            # "/var/lib/rundeck/data",            # database
+            # "/var/lib/rundeck/logs",            # execution logs (by far biggest)
+            # "/var/lib/rundeck/.ssh",            # ssh keys
+            # "/var/lib/rundeck/var/storage",     # key storage files and metadata
             "/var/rundeck/projects"             # project definitions
         ]
 
@@ -48,18 +46,16 @@ class Keeper:
                 zip_handle.write(os.path.join(root, f))
 
 
-    def backup(self):
-        # Refuse to do anything if RunDeck is running
-        if self._rundeck_is_running():
-            logging.error("rundeckd cannot be running while you take a backup")
-            raise Exception("rundeckd is still running")
+    def backup(self, destination_path, filename):
+        # Start message
+        logging.debug("starting backup")
 
         # Fail if backup dir is not found
-        if not os.path.exists(self.destination_path):
-            logging.error("backup directory {} not found".format(self.destination_path))
+        if not os.path.exists(destination_path):
+            logging.error("backup directory {} not found".format(destination_path))
             raise Exception("could not find backup directory")
 
-        file_path = os.path.join(self.destination_path, self.filename)
+        file_path = os.path.join(destination_path, filename)
         logging.debug("using full backup path {}".format(file_path))
 
         if self.show_progress:
@@ -79,17 +75,25 @@ class Keeper:
             for directory in self.source_directories:
                 logging.info("adding directory {}".format(directory))
                 self._add_directory_to_zip(zip_file, directory)
-                print("") # To drop get the progress bar on separate line from
+                print("") # To get the progress bar on separate line from
                           # log messages when printing log to stdout
 
         if self.show_progress:
             # Close progress bar
             self.bar.finish()
 
+    def restore(self, filename):
+        return
+
 def main(arguments):
+    # Refuse to do anything if RunDeck is running
+    # This is best practice according to the docs:
+    # http://rundeck.org/2.6.11/administration/backup-and-recovery.html
+    if self._rundeck_is_running():
+        logging.error("rundeckd cannot be running while you take a backup")
+        raise Exception("rundeckd is still running")
     # Gather arguments
-    destination_path = arguments.dest
-    filename = arguments.filename
+    parser_name = arguments.subparser_name
     debug_mode = arguments.debug
     if arguments.no_progress:
         show_progress = False
@@ -104,33 +108,44 @@ def main(arguments):
     # Set up logging
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',level=log_level)
 
-    # Start message
-    logging.debug("starting backup")
+    keeper = Keeper(show_progress=show_progress)
 
-    keeper = Keeper(destination_path=destination_path,
-                    filename=filename,
-                    show_progress=show_progress)
-
-    # Run backup
-    keeper.backup()
-
+    if parser_name == "backup":
+        # Run backup
+        keeper.backup(destination_path=arguments.dest,
+                      filename=arguments.filename)
+    elif parser_name == "restore":
+        keeper.restore(filename=arguments.file)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="rundeck-keeper: helper for backup and restore of RunDeck")
-    parser.add_argument("--debug",
-                        "-d",
+    parser = argparse.ArgumentParser(description='dinghy: helper for backup and restore of RunDeck')
+    parser.add_argument('--debug',
+                        '-d',
                         action='store_true',
                         help='enable debug logging')
-    parser.add_argument("--dest",
-                        type=str,
-                        required=True,
-                        help='path to write backup file to')
-    parser.add_argument("--filename",
-                        type=str,
-                        help='override the filename used the for backup file',
-                        default="rundeck-backup-{}.zip".format(
-                            datetime.now().strftime("%Y-%M-%d--%H-%m-%S")))
-    parser.add_argument("--no-progress",
+    parser.add_argument('--no-progress',
                         action='store_true',
                         help='disable progress bar')
+
+    subparsers = parser.add_subparsers(help='command help',
+                                       dest='subparser_name')
+
+    # Backup options
+    backup_parser = subparsers.add_parser('backup', help='create a backup')
+    backup_parser.add_argument('--dest',
+                                type=str,
+                                required=True,
+                                help='path to write backup file to')
+    backup_parser.add_argument('--filename',
+                                type=str,
+                                help='override the filename used the for backup file',
+                                default='rundeck-backup-{}.zip'.format(
+                            datetime.now().strftime('%Y-%M-%d--%H-%m-%S')))
+
+    # Restore options
+    restore_parsers = subparsers.add_parser('restore',
+                                            help='restore from a backup file')
+    restore_parser.add_argument('--file',
+                                type=str,
+                                help='path to backup file to restore from')
     main(parser.parse_args())
